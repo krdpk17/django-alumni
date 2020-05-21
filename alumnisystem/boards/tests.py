12a135787510f5f2faecc94c4a4e74adf33260e5
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-21 15:48:43 +0530

    Added test code
    Ref: https://simpleisbetterthancomplex.com/series/2017/09/18/a-complete-beginners-guide-to-django-part-3.html

--- alumnisystem/boards/tests.py
+++ alumnisystem/boards/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
+from .views import home, board_topics
+from .models import Board
 
 class HomeTests(TestCase):
     def test_home_view_status_code(self):
@@ -10,3 +12,21 @@ class HomeTests(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.url_name, 'home')
+
+class BoardTopicsTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+
+    def test_board_topics_view_success_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_board_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_board_topics_url_resolves_board_topics_view(self):
+        view = resolve('/boards/1/')
+        self.assertEquals(view.func, board_topics)
--- alumnisystem/boards/views.py
+++ alumnisystem/boards/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-
+from django.http import Http404
 from django.http import HttpResponse
 from .models import Board
+
 
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
-    
+
 def board_topics(request, pk):
-    board = Board.objects.get(pk=pk)
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
     return render(request, 'topics.html', {'board': board})
