d35bc82a7a469db7f45405fa1d1293992c17b0f7
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-21 17:33:30 +0530

    added new topic link
    https://simpleisbetterthancomplex.com/series/2017/09/18/a-complete-beginners-guide-to-django-part-3.html

--- alumnisystem/boards/tests.py
+++ alumnisystem/boards/tests.py
@@ -40,11 +40,15 @@ class BoardTopicsTests(TestCase):
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
         
-    def test_board_topics_view_contains_link_back_to_homepage(self):
+    def test_board_topics_view_contains_navigation_links(self):
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
-        response = self.client.get(board_topics_url)
         homepage_url = reverse('home')
+        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+
+        response = self.client.get(board_topics_url)
+        
         self.assertContains(response, 'href="{0}"'.format(homepage_url))
+        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
     
     def test_new_topic_view_contains_link_back_to_board_topics_view(self):
         new_topic_url = reverse('new_topic', kwargs={'pk': 1})
