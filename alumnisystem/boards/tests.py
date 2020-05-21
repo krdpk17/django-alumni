65e0e1b401ded4ea4d8be28377e9640d0e973d53
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-21 13:11:23 +0530

    added testcase for boards

--- alumnisystem/boards/tests.py
+++ alumnisystem/boards/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.url_name, 'home')
--- alumnisystem/boards/views.py
+++ alumnisystem/boards/views.py
@@ -5,11 +5,4 @@ from .models import Board
 
 def home(request):
     boards = Board.objects.all()
-    boards_names = list()
-
-    for board in boards:
-        boards_names.append(board.name)
-
-    response_html = '<br>'.join(boards_names)
-
-    return HttpResponse(response_html)
+    return render(request, 'home.html', {'boards': boards})
