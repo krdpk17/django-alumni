7a7c82e772fda3037faa99f74a3077899ac8b5b6
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-22 16:15:44 +0530

    logout mention in url and home page update for login option

--- alumnisystem/accounts/urls.py
+++ alumnisystem/accounts/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^update/$', views.UpdateProfile.as_view(), name='update'),
 ]
