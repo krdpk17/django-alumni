b03f5a5db83ffe10de0fcf342855ff9436c17587
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-22 06:01:48 +0530

    refactored html and added test cases

--- alumnisystem/boards/tests.py
+++ alumnisystem/boards/tests.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 from .views import home, board_topics
 from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 class HomeTests(TestCase):
     def setUp(self):
@@ -87,7 +88,9 @@ class NewTopicTests(TestCase):
         '''
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.post(url, {})
+        form = response.context.get('form')
         self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
 
     def test_new_topic_invalid_post_data_empty_fields(self):
         '''
@@ -103,3 +106,9 @@ class NewTopicTests(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
+
+    def test_contains_form(self): 
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
