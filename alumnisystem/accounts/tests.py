bf9f62608cae524714cd145646d56907e666eead
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-22 12:50:27 +0530

    added test case for signup form

--- alumnisystem/accounts/tests.py
+++ alumnisystem/accounts/tests.py
@@ -1,10 +1,17 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.urls import resolve
+from django.contrib.auth.forms import UserCreationForm
+
+from .forms import UserProfileForm
 
 import pdb
 
 class SignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
     def test_signup_status_code(self):
         url = reverse('signup')
         response = self.client.get(url)
@@ -19,3 +26,12 @@ class SignUpTests(TestCase):
         view = resolve('/accounts/signup/')
         print("output is {}".format(view.func))
         self.assertEquals(view.view_name, 'signup')
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        profile_form = self.response.context.get('profile_form')
+        self.assertIsInstance(form, UserCreationForm)
+        self.assertIsInstance(profile_form, UserProfileForm)
