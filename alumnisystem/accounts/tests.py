835f5968b44a6e5112e4f45791d1c30f52104559
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-22 13:22:33 +0530

    testcases for invalid signup

--- alumnisystem/accounts/tests.py
+++ alumnisystem/accounts/tests.py
@@ -72,3 +72,21 @@ class SuccessfulSignUpTests(TestCase):
         response = self.client.get(self.home_url)
         user = response.context.get('user')
         self.assertTrue(user.is_authenticated)
+
+class InvalidSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.post(url, {})  # submit an empty dictionary
+
+    def test_signup_status_code(self):
+        '''
+        An invalid form submission should return to the same page
+        '''
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_form_errors(self):
+        form = self.response.context.get('form')
+        self.assertTrue(form.errors)
+
+    def test_dont_create_user(self):
+        self.assertFalse(User.objects.exists())
