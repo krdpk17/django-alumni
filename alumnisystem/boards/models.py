68db0ed5595222d61facb30a87564fe3eecf6dd1
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-23 05:47:04 +0530

    feature:board page adds info about topics and posts

--- alumnisystem/boards/models.py
+++ alumnisystem/boards/models.py
@@ -1,19 +1,30 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.utils.text import Truncator
 
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.name
+
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+    def get_last_post(self):
+        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
+
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
     starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subject
 
 
 class Post(models.Model):
@@ -23,3 +34,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
     updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE)
+
+    def __str__(self):
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
