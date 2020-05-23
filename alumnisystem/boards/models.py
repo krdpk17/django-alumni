46657c9947221022ba0d82f4fcbb3d1016c87e54
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-23 13:35:39 +0530

    Add previous in navigation, Also added stripping

--- alumnisystem/boards/models.py
+++ alumnisystem/boards/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils.text import Truncator
 from django.utils.html import mark_safe
 from markdown import markdown
-
+import math
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
@@ -29,6 +29,21 @@ class Topic(models.Model):
     def __str__(self):
         return self.subject
 
+    def get_page_count(self):
+        count = self.posts.count()
+        pages = count / 20
+        return math.ceil(pages)
+
+    def has_many_pages(self, count=None):
+        if count is None:
+            count = self.get_page_count()
+        return count > 6
+
+    def get_page_range(self):
+        count = self.get_page_count()
+        if self.has_many_pages(count):
+            return range(1, 5)
+        return range(1, count + 1)
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
