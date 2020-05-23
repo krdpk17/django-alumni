82931eb463a443b8ae0bfc43cddbeab7e86af17e
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-23 13:22:28 +0530

    bugfix: View count should be per user session once. It is done here

--- alumnisystem/boards/views.py
+++ alumnisystem/boards/views.py
@@ -82,8 +82,12 @@ class PostListView(ListView):
     paginate_by = 2
 
     def get_context_data(self, **kwargs):
-        self.topic.views += 1
-        self.topic.save()
+        session_key = 'viewed_topic_{}'.format(self.topic.pk)
+        if not self.request.session.get(session_key, False):
+            self.topic.views += 1
+            self.topic.save()
+            self.request.session[session_key] = True 
+            
         kwargs['topic'] = self.topic
         return super().get_context_data(**kwargs)
 
@@ -105,7 +109,7 @@ def reply_topic(request, pk, topic_pk):
 
             topic.last_updated = timezone.now() 
             topic.save()
-            
+
             return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
     else:
         form = PostForm()
