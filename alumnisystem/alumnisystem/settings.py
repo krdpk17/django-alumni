ac9d692a95ada598af0d92eb9ad3d8145ced1672
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-25 13:22:46 +0530

    reverted env variable change as it is not working

--- alumnisystem/alumnisystem/settings.py
+++ alumnisystem/alumnisystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*$c5(+k28!2-pjjw9j=54_^k+x6v-^w5d_mcfg32w-$6rcbzw)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
